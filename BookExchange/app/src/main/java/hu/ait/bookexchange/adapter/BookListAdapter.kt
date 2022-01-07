d7e726d370538988a557443a7875c7bae5f19a9b
AnikaD15 <aduffus@princeton.edu>
2022-01-07 12:40:28 -0500

    Progress

--- BookExchange/app/src/main/java/hu/ait/bookexchange/adapter/BookListAdapter.kt
+++ BookExchange/app/src/main/java/hu/ait/bookexchange/adapter/BookListAdapter.kt
@@ -8,20 +8,23 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.make
 import com.google.firebase.firestore.FirebaseFirestore
 import hu.ait.bookexchange.BookListActivity
 import hu.ait.bookexchange.ClaimedBookActivity
 import hu.ait.bookexchange.R
 import hu.ait.bookexchange.data.Book
+import hu.ait.bookexchange.databinding.ActivityBookListBinding
 import hu.ait.bookexchange.databinding.BookListCardBinding
+import java.lang.Exception
 import java.sql.Time
 import java.util.*
 
-class BookListAdapter : RecyclerView.Adapter<BookListAdapter.ViewHolder>  {
+class BookListAdapter : RecyclerView.Adapter<BookListAdapter.ViewHolder> {
     lateinit var context: Context
     lateinit var currUserId: String
-    var  bookList = mutableListOf<Book>()
-    var  bookKeys = mutableListOf<String>()
+    var bookList = mutableListOf<Book>()
+    var bookKeys = mutableListOf<String>()
 
     val CLAIM_PERIOD = 3 // number of days for claim
 
@@ -35,10 +38,10 @@ class BookListAdapter : RecyclerView.Adapter<BookListAdapter.ViewHolder>  {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        val binding = BookItemBinding.inflate((context as BookListActivity).layoutInflater,
-//            parent, false)
-        val binding = BookListCardBinding.inflate((context as BookListActivity).layoutInflater,
-            parent, false)
+        val binding = BookListCardBinding.inflate(
+            (context as BookListActivity).layoutInflater,
+            parent, false
+        )
 
         return ViewHolder(binding)
     }
@@ -52,7 +55,7 @@ class BookListAdapter : RecyclerView.Adapter<BookListAdapter.ViewHolder>  {
         }
 
         holder.binding.btnClaim.setOnClickListener {
-            if(!bookList[holder.adapterPosition].isClaimed){
+            if (!bookList[holder.adapterPosition].isClaimed) {
                 claimBook(holder.adapterPosition, currUserId)
             }
         }
@@ -60,18 +63,38 @@ class BookListAdapter : RecyclerView.Adapter<BookListAdapter.ViewHolder>  {
 
     private fun claimBook(index: Int, currUserId: String) {
         // update book in database
-        bookList[index].isClaimed = true
-        bookList[index].claimedBy = currUserId
 
-        val calendar = Calendar.getInstance()
-        calendar.add(Calendar.DATE, CLAIM_PERIOD)
-        bookList[index].claimEndDate = calendar.time
+        try {
+            bookList[index].isClaimed = true
+            bookList[index].claimedBy = currUserId
 
-        notifyItemChanged(index)
-        val bookCollection = FirebaseFirestore.getInstance().collection(BookListActivity.COLLECTION_BOOKS)
-        bookCollection.document(bookKeys[index]).set(bookList[index])
+            val calendar = Calendar.getInstance()
+            calendar.add(Calendar.DATE, CLAIM_PERIOD)
+            bookList[index].claimEndDate = calendar.time
 
-        Toast.makeText(context as BookListActivity, (context as BookListActivity).getString(R.string.claim_end_date, bookList[index].claimEndDate.toString()), Toast.LENGTH_LONG).show()
+            notifyItemChanged(index)
+            val bookCollection =
+                FirebaseFirestore.getInstance().collection(BookListActivity.COLLECTION_BOOKS)
+            bookCollection.document(bookKeys[index]).set(bookList[index])
+
+            Toast.makeText(
+                context as BookListActivity, (context as BookListActivity)
+                    .getString(R.string.claim_end_date, bookList[index].claimEndDate.toString()),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        catch (e:Exception){
+            // remove claim
+            ClaimedBookAdapter(this.context, this.currUserId).unclaimBook(index)
+
+            Log.d("TAG_CLAIM", e.message.toString())
+
+            Toast.makeText(
+                context as BookListActivity, context.getString(R.string.claim_error),
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 
     private fun removeBook(index: Int) {
@@ -93,26 +116,29 @@ class BookListAdapter : RecyclerView.Adapter<BookListAdapter.ViewHolder>  {
         }
     }
 
-    fun addBook(book:Book, key:String){
+    fun addBook(book: Book, key: String) {
         bookList.add(book)
         bookKeys.add(key)
         notifyItemInserted(bookList.lastIndex)
     }
 
-    fun updateBook(book:Book, key:String){
+    fun updateBook(book: Book, key: String) {
         var index = bookKeys.indexOf(key)
         bookList[index] = book
         notifyItemChanged(index)
 
-        val bookCollection = FirebaseFirestore.getInstance().collection(BookListActivity.COLLECTION_BOOKS)
+        val bookCollection =
+            FirebaseFirestore.getInstance().collection(BookListActivity.COLLECTION_BOOKS)
         bookCollection.document(bookKeys[index]).set(bookList[index])
     }
 
-    inner class ViewHolder(var binding: BookListCardBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind(book: Book){
+    inner class ViewHolder(var binding: BookListCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(book: Book) {
             binding.tvTitle.text = book.title
             binding.tvAuthor.text = book.author
-            binding.tvPrice.text = (context as BookListActivity).getString(R.string.price, book.price)
+            binding.tvPrice.text =
+                (context as BookListActivity).getString(R.string.price, book.price)
             binding.tvCondition.text = (context as BookListActivity).resources
                 .getStringArray(R.array.condition_array)[book.condition]
         }
