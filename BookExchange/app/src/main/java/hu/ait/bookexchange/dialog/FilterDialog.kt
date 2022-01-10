b60601ebd0596c79f39abdda67816ecc5550e2c8
AnikaD15 <aduffus@princeton.edu>
2022-01-09 20:03:20 -0500

    Modifying filter dialog

--- BookExchange/app/src/main/java/hu/ait/bookexchange/dialog/FilterDialog.kt
+++ BookExchange/app/src/main/java/hu/ait/bookexchange/dialog/FilterDialog.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import hu.ait.bookexchange.R
 import hu.ait.bookexchange.data.Book
 import hu.ait.bookexchange.data.Search
+import hu.ait.bookexchange.databinding.BookDialogBinding
+import hu.ait.bookexchange.databinding.FilterDialogBinding
 
 class FilterDialog : BottomSheetDialogFragment() {
 
@@ -19,6 +23,9 @@ class FilterDialog : BottomSheetDialogFragment() {
     companion object {
         const val TAG = "FilterDialog"
     }
+
+    lateinit var filterDialogBinding: FilterDialogBinding
+    lateinit var filter: Search
 
 //    override fun onCreateView(
 //        inflater: LayoutInflater,
@@ -32,6 +39,23 @@ class FilterDialog : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View?
     {
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+        filterDialogBinding = FilterDialogBinding.inflate(layoutInflater)
+        dialogBuilder.setView(filterDialogBinding.root)
+
+        // set filter binding contents
+        // book condition spinner
+        val conditionAdapter = ArrayAdapter.createFromResource(
+            requireActivity(),
+            R.array.search_condition_array,
+            android.R.layout.simple_spinner_item)
+
+        conditionAdapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        filterDialogBinding.spinnerConditions.adapter = conditionAdapter
+
+        // title
         return inflater.inflate(R.layout.filter_dialog, container, false)
     }
 
